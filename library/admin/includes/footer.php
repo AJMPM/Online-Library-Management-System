a804747ad7eaf04a2fba85f75124f2fa098bca47
Parvez Musharrof <37952450+AJMPM@users.noreply.github.com>
2018-05-04 04:37:15 +0600

    Update footer.php

--- library/admin/includes/footer.php
+++ library/admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2017 Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : PHPGURUKUL</a> 
+                   &copy;Online Library Management System |<a> Designed by :Parvez Musharrof & Habibullah Rakib</a> 
                 </div>
 
             </div>
